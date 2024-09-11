cebaa97daf422ce1b226ecce3e4e0cbb2fc58b87
Carry3033 <carry3033@qq.com>
2024-09-11 22:01:00 +0800

    chore: 开发模式与生产模式区分

--- webpack.config.js
+++ webpack.config.js
@@ -2,8 +2,10 @@ const path = require('path')                // 引入node内置模块path
 const htmlwebpackPlugin = require('html-webpack-plugin') // 构建HTML文件，该插件不仅可以加载HTML文件，还可以自动注入JavaScript和CSS文件
 const VueLoaderPlugin = require('vue-loader/lib/plugin') // 构建vue文件
 
+const isDev = process.env.NODE_ENV === 'development';
+
 module.exports ={
-  mode: "development",                      // 开发模式
+  mode: isDev ? 'development' : 'production',
   entry:'./src/main.js',                    // 入口文件，entry 是告诉 Webpack 从哪里开始构建你的应用程序
   output:{                                  // 出口文件
     path:path.resolve(__dirname,'dist'),    // 出口路径和目录
@@ -12,7 +14,7 @@ module.exports ={
   devServer: {
     port: 8080,                             // 端口
     open: true,                             // 运行自动打开浏览器
-    hot: true,                              //热服务
+    hot: true                               // 热服务
   },
   module: {
     // 注意执行顺序，loader 的执行顺序是从下到上，从右到左
